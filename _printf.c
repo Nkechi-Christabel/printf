df7eee9862afed6a305bad1d96f882c1fb038867
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-07-22 15:19:26 -0700

    Used a local buffer of 1024 chars in order to call write as little as possible.

--- _printf.c
+++ _printf.c
@@ -6,15 +6,17 @@
  * check_str - Prints a string
  * @str: The string to print
  * @count: The number of characters printed
+ * @buffer: The buffer to store characters
+ * @buffer_index: The current index in the buffer
  */
-void check_str(char *str, int *count)
+void check_str(char *str, int *count, char *buffer, int *buffer_index)
 {
 	if (!(*str))
 		str = "(null)";
 
 	while (*str)
 	{
-		_putchar(*str);
+		_putchar(*str, count, buffer, buffer_index);
 		str++;
 		(*count)++;
 	}
@@ -26,46 +28,49 @@ void check_str(char *str, int *count)
  * @format: A list of types of arguments passed to the function
  * @args: The arguments to be printed
  * @count: The number of characters printed
+ * @buffer: The buffer to store characters
+ * @buffer_index: The current index in the buffer
  */
-void print_arg(const char *format, int *count, va_list args)
+void print_arg(const char *format, int *count, va_list args, char *buffer,
+		int *buffer_index)
 {
 	char *str;
 
 	switch (*format)
 	{
 		case 'c':
-			_putchar(va_arg(args, int));
+			_putchar(va_arg(args, int), count, buffer, buffer_index);
 			(*count)++;
 			break;
 		case 's':
 			str = va_arg(args, char *);
-			check_str(str, count);
+			check_str(str, count, buffer, buffer_index);
 			break;
 		case '%':
-			_putchar('%');
+			_putchar('%', count, buffer, buffer_index);
 			(*count)++;
 			break;
 		case ('d' || 'i'):
-			print_number(va_arg(args, int), count);
+			print_number(va_arg(args, int), count, buffer, buffer_index);
 			break;
 		case 'b':
-			print_binary(va_arg(args, unsigned int), count);
+			print_binary(va_arg(args, unsigned int), count, buffer, buffer_index);
 			break;
 		case 'u':
-			print_ui(va_arg(args, unsigned int), count);
+			print_ui(va_arg(args, unsigned int), count, buffer, buffer_index);
 			break;
 		case 'o':
-			print_octal(va_arg(args, unsigned int), count);
+			print_octal(va_arg(args, unsigned int), count, buffer, buffer_index);
 			break;
 		case 'x':
-			print_hex(va_arg(args, unsigned int), 0, count);
+			print_hex(va_arg(args, unsigned int), 0, count, buffer, buffer_index);
 			break;
 		case 'X':
-			print_hex(va_arg(args, unsigned int), 1, count);
+			print_hex(va_arg(args, unsigned int), 1, count, buffer, buffer_index);
 			break;
 		default:
-			_putchar('%');
-			_putchar(*format);
+			_putchar('%', count, buffer, buffer_index);
+			_putchar(*format, count, buffer, buffer_index);
 			(*count) += 2;
 			break;
 	}
@@ -80,6 +85,8 @@ void print_arg(const char *format, int *count, va_list args)
 int _printf(const char *format, ...)
 {
 	int count = 0;
+	int buffer_index = 0;
+	char buffer[BUFFER_SIZE];
 	va_list args;
 
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
@@ -91,17 +98,19 @@ int _printf(const char *format, ...)
 	{
 		if (*format != '%')
 		{
-			_putchar(*format);
-			count++;
+			_putchar(*format, &count, buffer, &buffer_index);
 		}
 		else
 		{
 			format++;
-			print_arg(format, &count, args);
+			print_arg(format, &count, args, buffer, &buffer_index);
 		}
 
 		format++;
 	}
+
+	if (buffer_index > 0)
+		_write_buffer(buffer, &count, &buffer_index);
 
 	va_end(args);
 
