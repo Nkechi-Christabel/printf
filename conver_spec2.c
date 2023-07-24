11930471bdfdbd2634cbb940389cc1e2212a9cd3
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-07-24 02:27:16 -0700

    Updated the function that handles the conversion specifier: p.

--- conver_spec2.c
+++ conver_spec2.c
@@ -82,6 +82,6 @@ void print_ptr(void *p, int *count, char *buffer, int *buffer_index)
 		ptr_val /= 16;
 	}
 	buffer[(*buffer_index) + num_chars] = '\0';
-	(*buffer_index) += num_chars;
-	(*count) += num_chars;
+	(*buffer_index) += num_chars + 1;
+	(*count) += num_chars + 3;
 }
