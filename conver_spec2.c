4baccd9f72c459908bb1432010fad7a5f2368f2d
Nkechi-Christabel <udenkwornkechi@gmail.com>
2023-07-26 00:08:08 -0700

    Updated th print pointer function with a check for NULL

--- conver_spec2.c
+++ conver_spec2.c
@@ -50,9 +50,9 @@ void print_ptr(void *p, int *count, char *buffer, int *buffer_index)
 	unsigned int num_chars = 0;
 	unsigned int digit;
 	const char hexString[] = "0123456789abcdef";
-	char *str = "(nil)(nil)";
+	char *str = "(nil)";
 
-	if (!p)
+	if (p == NULL)
 	{
 		while(*str)
 		{
